9534a0ba6b0bab85684aa549dd83bbf15c03d7e1
Feliciano Acevedo <feliciano.acevedo.trebbau@gmail.com>
2025-02-12 10:07:45 +0100

    Flashlight test

---
+++ Source/Stalker2/Public/S_LightPresset_Radius.hpp
@@ -0,0 +1,18 @@
+#ifndef UE4SS_SDK_S_LightPresset_Radius_HPP
+#define UE4SS_SDK_S_LightPresset_Radius_HPP
+
+struct FS_LightPresset_Radius
+{
+    FName PresetName_34_C1D83792488CFFDA2C21A3AAF90EE6EA;                             // 0x0000 (size: 0x8)
+    double Intensity_24_B7457FA4450E0594B8730DB897C90029;                             // 0x0008 (size: 0x8)
+    double AttenuationRadius_55_F7F4985C4E2287E41B4C6C89997A2F09;                     // 0x0010 (size: 0x8)
+    double EmissiveBrightness_30_10099DA34E135293FEE5FCBCEA5E7166;                    // 0x0018 (size: 0x8)
+    double EmissivePower_31_CAC08C9540A7712284335A8736B80B6E;                         // 0x0020 (size: 0x8)
+    double LightBeamBrightness_32_7BC012CD490A9BA4F4F8CF99377E192C;                   // 0x0028 (size: 0x8)
+    double LightBeamPower_33_B2D587144F8BCFC3B91C20A3CE419B3D;                        // 0x0030 (size: 0x8)
+    double EmissiveIntensity_60_AA01FE2D444453FC85956D91D5670198;                     // 0x0038 (size: 0x8)
+    double GlowIntensity_65_F8EE94514C4C4FE78820FD86D3984FA1;                         // 0x0040 (size: 0x8)
+
+}; // Size: 0x48
+
+#endif
---
+++ Source/Stalker2/Public/S_LightPresset_Radiusd.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/UserDefinedStruct.h"
+#include "S_LightPresset_Radiusd.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STALKER2_API US_LightPresset_Radiusd : public UUserDefinedStruct
+{
+	GENERATED_BODY()
+	
+};
